bd438c96f16583c4646376c3dbefe4016e2aeac9
lilyzazalak <lilyzazalak@gmail.com>
2017-09-21 15:40:44 -0700

    Assignment 1.2

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,9 +2,9 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
-
-
-
+for( var i = 1; i<=100; i++){
+    console.log(i)
+}
 /*
 Exercise #2
 Write a loop that makes seven calls to console.log to output the following triangle:
@@ -16,4 +16,9 @@ Write a loop that makes seven calls to console.log to output the following trian
 #####
 ######
 #######
-*/
+*/var x="#"
+var y="#######"
+while(x<=y){
+    console.log(x);
+    x=x+"#";
+}
